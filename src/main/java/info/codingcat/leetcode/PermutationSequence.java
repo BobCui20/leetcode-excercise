914781d5f043fe781aca015637703dcd35e8bd95
BobCui20 <zhijiecui@outlook.com>
2020-01-18 23:52:30 -0500

    update of var names

--- src/main/java/info/codingcat/leetcode/PermutationSequence.java
+++ src/main/java/info/codingcat/leetcode/PermutationSequence.java
@@ -8,7 +8,6 @@ import java.util.List;
  */
 public class PermutationSequence {
 
-
     public String getPermutation(int n, int k) {
         StringBuilder s = new StringBuilder();
         List<Integer> selection = new ArrayList<>(n);
@@ -16,16 +15,15 @@ public class PermutationSequence {
             selection.add(i);
         }
         for (int i = n; i > 0; i--) {
-            int currNumOfDigit = fac(i - 1);
-            int digit = (k - 1)/currNumOfDigit + 1;
+            int currNumOfSelections = fac(i - 1);
+            int digit = (k - 1) / currNumOfSelections + 1;
 
-            k = k%currNumOfDigit;
-            if(k == 0) {
-                k = currNumOfDigit;
+            k = k % currNumOfSelections;
+            if (k == 0) {
+                k = currNumOfSelections;
             }
             s.append(selection.get(digit - 1));
             selection.remove(digit - 1);
-            
 
         }
         return s.toString();
@@ -33,7 +31,7 @@ public class PermutationSequence {
 
     public int fac(int n) {
         int r = 1;
-        while(n != 0) {
+        while (n != 0) {
             r *= n--;
         }
         return r;
